972ba14ae83f738255fc616eda31c4ca91c7e57d
hyejun8701 <hyejun8701@gmail.com>
2018-04-10 11:50:05 +0900

    host

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    HOST: "/external",
+    HOST: "http://api.stepin.xmerce.com/external/smt",
     uploadPath: 'http://api.stepin.xmerce.com/upload'
 };
