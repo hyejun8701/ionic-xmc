65f312b7cc351258675c5f404e6bef10d793b904
hyejun8701 <hyejun8701@gmail.com>
2018-04-10 13:32:04 +0900

    host

--- src/providers/base-provider.ts
+++ src/providers/base-provider.ts
@@ -6,7 +6,8 @@ export class BaseProvider {
     protected headers: HttpHeaders;
 
     constructor() {
-        this.SERVER = `${environment.HOST}`;
+        //this.SERVER = `${environment.HOST}`;
+        this.SERVER = 'http://api.stepin.xmerce.com/external/smt';
         this.headers = new HttpHeaders({
             'Access-Control-Allow-Origin': '*',
             'Content-Type': 'application/json',
