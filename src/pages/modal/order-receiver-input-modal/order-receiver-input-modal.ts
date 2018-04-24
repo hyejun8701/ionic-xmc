49c4ec02f6bb1745521863ce84b9db89b601e2ef
hyejun8701 <hyejun8701@gmail.com>
2018-04-24 16:17:29 +0900

    input add remove

--- src/pages/modal/order-receiver-input-modal/order-receiver-input-modal.ts
+++ src/pages/modal/order-receiver-input-modal/order-receiver-input-modal.ts
@@ -19,22 +19,20 @@ export class OrderReceiverInputModalPage {
               private alertCtrl: AlertController) {
     this.alreadyUse = navParams.get('receivers');
     this.datas = ["", "", ""];
-    this.possibleCnt = (GlobalConstants.RECEIVER_POSSIBLE_COUNT_DEFAULT - this.alreadyUse.length) - this.datas.length;
+    this.possibleCnt = GlobalConstants.RECEIVER_POSSIBLE_COUNT_DEFAULT - this.alreadyUse.length;
+  }
+
+  receiversMaxOverAlert() {
+    let alert = this.alertCtrl.create({
+      subTitle: '수신자는 최대 10명 입니다.',
+      buttons: [{text: '확인'}]
+    });
+    alert.present();
   }
 
   inputAdd() {
-    if(this.possibleCnt === 0) {
-      let alert = this.alertCtrl.create({
-        subTitle: '수신자는 최대 10명 입니다.',
-        buttons: [
-          {
-            text: '확인',
-            handler: () => {
-            }
-          }
-        ]
-      });
-      alert.present();
+    if(this.possibleCnt < 1) {
+      this.receiversMaxOverAlert();
     } else {
       this.datas.push("");
       this.possibleCnt--;
@@ -43,17 +41,34 @@ export class OrderReceiverInputModalPage {
   
   inputFocusout(event: any, data) {
     let value = event.target.value;
-    if (value && value.trim() !== '') {
+
+    console.log("================> " + this.possibleCnt);
+
+    let remainCnt = 0;
+    
+    this.datas.forEach((data) => {
+      if(data === "") {
+        remainCnt++;
+      }
+    });
+
+    console.log("remainCnt => " + remainCnt);
+
+    if((this.possibleCnt + remainCnt) < 1) {
+      this.receiversMaxOverAlert();
+      event.target.value = "";
+    } else if (value && value.trim() !== '') {
       value = CommonFuntions.fnChangeToCallNumberFormat(value);
       
       let alreadyCnt = 0;
-      this.datas.forEach((element, idx) => {
-        if(element[idx] === value) {
-          alreadyCnt++;
-        }
-      });
+      // this.datas.forEach((element, idx) => {
+      //   if(element[idx] === value) {
+      //     alreadyCnt++;
+      //   }
+      // });
 
-      //2. already cnt check alert
+      //1. already cnt check alert
+      //2. already + datas length, add/remove input  check
 
       if((this.alreadyUse.indexOf(value) > -1) || (alreadyCnt > 1)) {
         let alert = this.alertCtrl.create({
@@ -86,7 +101,12 @@ export class OrderReceiverInputModalPage {
 
   inputRemove(i) {
     this.datas.splice(i , 1);
+    
+    console.log("this.datas.length => " + this.datas.length);
+
     this.possibleCnt = (GlobalConstants.RECEIVER_POSSIBLE_COUNT_DEFAULT - this.alreadyUse.length) - this.datas.length;
+
+    console.log("this.possibleCnt => " + this.possibleCnt);
   }
 
   dismiss() {
--- src/pages/modal/order-send-modal/order-send-modal.ts
+++ src/pages/modal/order-send-modal/order-send-modal.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams, ModalController, ViewController, A
 import { OrderSendProvider } from '../../../providers/order/order-send';
 import { BtobLoginProvider } from '../../../providers/btob/btob-login';
 import { BtobMemberCreditProvider } from '../../../providers/btob/btob-member-credit';
+import * as GlobalConstants from '../../../common/global-constants';
 
 @IonicPage()
 @Component({
@@ -25,37 +26,49 @@ export class OrderSendModalPage {
               private alertCtrl: AlertController
             ) {
     this.goods = navParams.get("item");
-    //console.log(this.goods);
   }
 
   createReceiverModal(type) {
-    //console.log(type);
-    if(type == 'input') {
-      let modal = this.modalCtrl.create('OrderReceiverInputModalPage', {receivers: this.receivers});
-      modal.onDidDismiss(data => {
-        if(data != null) {
-          for(let i = 0; i < data.length; i++) {
-            if(!data[i]) {
-              continue;
+    if(type != null && (this.receivers.length === GlobalConstants.RECEIVER_POSSIBLE_COUNT_DEFAULT)) {
+      let alert = this.alertCtrl.create({
+        subTitle: '수신자는 최대 10명 입니다.',
+        buttons: [
+          {
+            text: '확인',
+            handler: () => {
             }
-            this.receivers.push(data[i]);
           }
-        }
+        ]
       });
-      modal.present();
-    } else if(type == 'address') {
-      let modal = this.modalCtrl.create('OrderReceiverContactsModalPage', {receivers: this.receivers});
-      modal.onDidDismiss(data => {// 추후 이름표시 요구 없을경우 onDidDismiss 를 input type modal 과 통합
-        if(data != null) {
-          for(let i = 0; i < data.length; i++) {
-            if(!data[i]) {
-              continue;
+      alert.present();
+    } else {
+      if(type == 'input') {
+        let modal = this.modalCtrl.create('OrderReceiverInputModalPage', {receivers: this.receivers});
+        modal.onDidDismiss(data => {
+          if(data != null) {
+            for(let i = 0; i < data.length; i++) {
+              if(!data[i]) {
+                continue;
+              }
+              this.receivers.push(data[i].replace(/\-/g, ''));
             }
-            this.receivers.push(data[i].phoneNumber);
           }
-        }
-      });//
-      modal.present();
+        });
+        modal.present();
+      } else if(type == 'address') {
+        let modal = this.modalCtrl.create('OrderReceiverContactsModalPage', {receivers: this.receivers});
+        modal.onDidDismiss(data => {// 추후 이름표시 요구 없을경우 onDidDismiss 를 input type modal 과 통합
+          if(data != null) {
+            for(let i = 0; i < data.length; i++) {
+              if(!data[i]) {
+                continue;
+              }
+              this.receivers.push(data[i].phoneNumber.replace(/\-/g, ''));
+            }
+          }
+        });//
+        modal.present();
+      }
     }
   }
 
